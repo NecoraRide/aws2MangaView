c54d02fa15ca0545ebce99377785e93ef935d9a0
ridehouse <ridehouse.street@gmail.com>
2016-01-18 19:30:33 +0100

    Commit de los uploads

--- modulos/funional/subirArchivo.php
+++ modulos/funional/subirArchivo.php
@@ -1,20 +1,25 @@
-<? 
-//tomo el valor de un elemento de tipo texto del formulario 
-$cadenatexto = $_POST["cadenatexto"]; 
-echo "Escribió en el campo de texto: " . $cadenatexto . "<br><br>"; 
+<?php
+    //funcion de descompresion
+    function foo($nombreZip){
+        $zip = new ZipArchive;
+        if ($zip->open($nombreZip) === TRUE) {
+            $zip->extractTo('../../Mangas/temp/'+$nombreZip);
+            $zip->close();
+            echo 'ok';
+        } else {
+            echo 'failed';
+        }
+    }
+    if (is_uploaded_file($HTTP_POST_FILES['archivo']['tmp_name'])) {
+      copy($HTTP_POST_FILES['archivo']['tmp_name'], $HTTP_POST_FILES['archivo']['name']);
+      $subio = true;
+    }
 
-//datos del arhivo 
-$nombre_archivo = $HTTP_POST_FILES['userfile']['name']; 
-$tipo_archivo = $HTTP_POST_FILES['userfile']['type']; 
-$tamano_archivo = $HTTP_POST_FILES['userfile']['size']; 
-//compruebo si las características del archivo son las que deseo 
-if (!((strpos($tipo_archivo, "gif") || strpos($tipo_archivo, "jpeg")) && ($tamano_archivo < 100000))) { 
-   	echo "La extensión o el tamaño de los archivos no es correcta. <br><br><table><tr><td><li>Se permiten archivos .gif o .jpg<br><li>se permiten archivos de 100 Kb máximo.</td></tr></table>"; 
-}else{ 
-   	if (move_uploaded_file($HTTP_POST_FILES['userfile']['tmp_name'], $nombre_archivo)){ 
-      	echo "El archivo ha sido cargado correctamente."; 
-   	}else{ 
-      	echo "Ocurrió algún error al subir el fichero. No pudo guardarse."; 
-   	} 
-} 
+    if($subio) {
+        echo "El archivo subio con exito";
+    } else {
+        echo "El archivo no cumple con las reglas establecidas";
+    }
+die();
+
 ?>
--- modulos/upload.php
+++ modulos/upload.php
@@ -1,16 +1,6 @@
-<form action="subearchivo.php" method="post" enctype="multipart/form-data"> 
-    <p>El manga tiene que estar comprimido en formato ZIP, dentro del ZIP tienen que estar los 
-        capitulos en diferentes carpetas numerasdas del 1 hasta el capitulo final a subir.
-    </p>
-   	<b>Nombre del manga:</b> 
-   	<p/>
-   	<input type="text" name="cadenatexto" size="20" maxlength="100"> 
-   	<input type="hidden" name="MAX_FILE_SIZE" value="100000"> 
-   	<br> 
-   	<br> 
-   	<b>Enviar un nuevo archivo: </b> 
-   	<br> 
-   	<input name="userfile" type="file"> 
-   	<br> 
-   	<input type="submit" value="Enviar">
+<form action="funional/subirArchivo.php" method="post" enctype="multipart/form-data" name="form1">
+  <p align="center">Archivo
+   <input name="archivo" type="file" id="archivo">
+  </p>
+  <p align="center"><input name="boton" type="submit" id="boton" value="Enviar"></p>
 </form>
