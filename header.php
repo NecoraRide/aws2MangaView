d078d4e986fe04c20e827de930ce9d7b1946b206
gpreislp <gpreisgammer@gmail.com>
2016-01-22 00:58:17 +0100

    Modificaciones css

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 		<!-- LOGIN
 			<script src="js/auth.js"></script>
 		-->
+        <?php session_start(); ?>
 		<title>Home</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
