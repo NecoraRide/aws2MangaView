d6c67fab03cd2dceb1f126cb8707aa6e801dfe94
unknown <AWS2-18@HPi5-18.infodom.web>
2016-01-25 19:27:16 +0100

    CSS Footer modificado + Mangas.php

---
+++ mangas.php
@@ -0,0 +1,27 @@
+<?php include('header.php') ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="espaciadoTop"></div>
+                <h1>ULTIMOS MANGAS A&Ntilde;ADIDOS</h1>
+                <div class="zonaMangas">
+                        <?php
+                            $fichero = fopen("./modulos/ficheros/mangas.txt","r");
+                            while(!feof($fichero)):
+                                $tmp = fgets($fichero,4096);
+                                $manga = split(":", $tmp);
+                                for($i = 0; $i < sizeof($manga)-1; $i++):
+                                ?>
+                                        <label><?php echo $manga[$i];?></label>
+                                        <img class="manga" src="Mangas/test.png"></img>
+                                        <img class="separacion "src="img/separador2.png"></img>
+
+                            <?php endfor;
+                            endwhile;
+                        ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include('footer.php')	?>
