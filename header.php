91038eede299ec2b2c2f31f43568de5e3cb71111
GPreisLP <gpreisgammer@gmail.com>
2016-01-25 18:42:38 +0100

    CSS
    CSS

--- header.php
+++ header.php
@@ -4,6 +4,15 @@
 		<!-- LOGIN
 			<script src="js/auth.js"></script>
 		-->
+        <?php class mangaView
+            {
+                function baseUrl()
+                {
+                    echo "http://localhost/aws2MangaView/";
+                }
+            }
+            $mangaView = new mangaView;
+        ?>
 		<title>Home</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php include('header.php') ?>
-	<div class="col-sm-12 col-xs-12 col-md-9 col-lg-9">
-		<div id="espaciadoTop"></div>
-		<h1>ULTIMOS MANGAS A&Ntilde;ADIDOS</h1>
-		<div class="zone">
-            <img class="separacion "src="img/separador2.png"></img>
-			<?php for($i=0;$i<5;$i++) :?>
-				<img class="manga" src="Mangas/test.png"></img>
-				<img class="separacion "src="img/separador2.png"></img>
-			<?php endfor; ?>
-            <p></p>
-            <?php for($i=0;$i<5;$i++) :?>
-				manga <?php echo ($i+1); ?>
-			<?php endfor; ?>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="espaciadoTop"></div>
+                <h1>ULTIMOS MANGAS A&Ntilde;ADIDOS</h1>
+                <div class="zone">
+                    <?php for($i=0;$i<5;$i++) :?>
+                        <div class="col-md-1">
+                            <img class="manga" src="Mangas/test.png"></img>
+                            <img class="separacion "src="img/separador2.png"></img>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+	
 <?php include('footer.php')	?>
