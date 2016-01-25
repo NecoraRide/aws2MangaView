319eab41f797838c788f09a76b9e8002de93bdd2
ridehouse <ridehouse.street@gmail.com>
2016-01-25 19:27:26 +0100

    arreglo extra

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
-        <!--link rel="stylesheet" href="css/carrousel.css" /-->
+        <link rel="stylesheet" href="css/carrousel.css" />
         <link rel="stylesheet" href="css/manga.css" />
 		<link rel="shortcut icon" type="image/png" href="img/favicon.png">
 		<link rel="stylesheet" href="css/bootstrap.css" />
