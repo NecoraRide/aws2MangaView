8656439ea634364a3e4827bab8e0509c3441e3e4
gpreislp <gpreisgammer@gmail.com>
2016-01-31 19:06:58 +0100

    Listado de tomos o mangas

--- list.php
+++ list.php
@@ -1,34 +1,30 @@
 <?php include('header.php') ?>
-    <script>
-        jQuery(document).ready(function(){
-            var x = window.location.href.split('=');
-            var manga;
-            switch(x[1]){
-                case 'assassination_classroom':
-                    manga = 'Assassination Classroom';
-                    break;
-                case 'death_note':
-                    manga = 'Death Note';
-                    break;
-                case 'high_school_of_the_dead':
-                    manga = 'High School Of The Dead';
-                    break;
-            }
-
-            jQuery('#nombreManga').html(manga);
-
-
-        });
-    </script>
-
+    <?php
+        $r = $_SERVER['REQUEST_URI'];
+        $r = explode('=', $r);
+        $r = array_filter($r);
+        $r = array_merge($r, array());
+        $endofurl = $r[1];
+        $manga = $r[1];
+    ?>
      <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div id="espaciadoTop"></div>
-                <h1>TOMOS <span id="nombreManga"></span></h1>
-                <div class="zonaMangas">
+                <h1>TOMOS <?php echo $manga; ?></h1>
+                <?php
+                    $dir = opendir('./Mangas/'.$manga);
+                    while ($elemento = readdir($dir)):
+                        if( $elemento != "." && $elemento != ".." && $elemento != 'portada.jpg'):?>
 
-                </div>
+                                <div class="col-md-4">
+                                    <img src="Mangas/Assassination_Classroom/portada.jpg" alt="Assassination Classroom" style="width:5%, height:40px;">
+                                    <?php echo $elemento;?>
+                                </div>
+                        <?php
+                        endif;
+                    endwhile;
+                ?>
             </div>
         </div>
     </div>
